41b7dd3c723586826542265bdc074f0a49e76828
jiristrouhal <MznStrouhal@gmail.com>
2025-12-29 06:46:22 +0100

    Add module

---
+++ process.py
@@ -0,0 +1,30 @@
+import dataclasses
+from collections import defaultdict
+
+from read import Transaction
+
+
+@dataclasses.dataclass
+class Result:
+    total_incomes: dict[str, float]
+    total_expenses: dict[str, float]
+    zeros: dict[str, float]
+
+
+def process_transactions(data: list[Transaction], days: int) -> Result:
+    totals: dict[str, float] = defaultdict(float)
+
+    for transaction in data:
+        totals[transaction.category] += transaction.amount * (30 / days)
+
+    total_incomes = {
+        k: v for k, v in sorted(totals.items(), key=lambda item: item[1], reverse=True) if v > 0
+    }
+    total_expenses = {k: v for k, v in sorted(totals.items(), key=lambda item: item[1]) if v < 0}
+    zeros = {k: v for k, v in totals.items() if v == 0}
+
+    return Result(
+        total_incomes=total_incomes,
+        total_expenses=total_expenses,
+        zeros=zeros,
+    )
