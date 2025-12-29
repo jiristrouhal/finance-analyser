57bb051dc201b7b6e893b604163af3c3495e4e4f
jiristrouhal <MznStrouhal@gmail.com>
2025-12-29 06:55:38 +0100

    Fix handling zeros

--- process.py
+++ process.py
@@ -15,13 +15,13 @@ def process_transactions(data: list[Transaction], days: int) -> Result:
     totals: dict[str, float] = defaultdict(float)
 
     for transaction in data:
-        totals[transaction.category] += transaction.amount * (30 / days)
+        totals[transaction.category] += transaction.amount * (30.0 / days)
 
     total_incomes = {
         k: v for k, v in sorted(totals.items(), key=lambda item: item[1], reverse=True) if v > 0
     }
     total_expenses = {k: v for k, v in sorted(totals.items(), key=lambda item: item[1]) if v < 0}
-    zeros = {k: v for k, v in totals.items() if v == 0}
+    zeros = {k: v for k, v in totals.items() if abs(v) < 0.01}
 
     return Result(
         total_incomes=total_incomes,
